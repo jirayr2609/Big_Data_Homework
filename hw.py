0a6a2a70411566d0a83284e6b9569b3ffe49b57c
Zhirayr Abrahamyan <zabraha000@citymail.cuny.edu>
2018-04-26 15:19:41 -0400

    changes

--- hw.py
+++ hw.py
@@ -1,13 +1,21 @@
-import pyspark
-if__name__=='__main__':
-    sc=pyspark.SparkContext()
-    data = '/data/share/bdm/nyc_restaurants.csv', use_unicode=False).cache()
-    df=spark.read.load(data,
-                  format = 'csv',
-                  header=True,
-                  inferScheme=True).cache()
-    cDescription = df.na.drop(subset=['CUISINE DESCRIPTION'])
-    cDescription = cDescription.groupBy('CUISINE DESCRIPTION').count().sort('count',ascending = False)
-    
+from pyspark import SparkContext
 
-    cDescription.rdd.saveAstextFile('output') 
+if __name__ == '__main__':
+	sc = SparkContext()
+
+	rest = sc.textFile('/data/share/bdm/nyc_restaurants.csv', use_unicode=False).cache()
+	# rest = sc.textFile('nyc_restaurants.csv', use_unicode=False).cache()
+
+
+	def get_count(partId, records):
+	    if partId==0:
+	        records.next()
+	        import csv
+	        reader = csv.reader(records)
+	        for row in reader:
+	            (description)=(row[7])
+	            yield (description,1)
+	restaurants = rest.mapPartitionsWithIndex(get_count)
+	restaurants.take(5)
+
+	count = restaurants.reduceByKey(lambda x,y: x+y).saveAsTextFile("output")
